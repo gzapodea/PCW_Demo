70053850c3f5d53c2e02259206b3b6aaafb72ddc
gzapodea <gzapodea@cisco.com>
2018-04-25 16:16:50 -0700

    updated files

---
+++ config.py
@@ -0,0 +1,23 @@
+
+
+# developed by Gabi Zapodeanu, TSA, GPO, Cisco Systems
+
+
+# This file contains the Spark Auth, DNAC user name and password, ServiceNow developer instance info
+
+
+SPARK_URL = 'https://api.ciscospark.com/v1'
+SPARK_AUTH = 'Bearer ' + 'Your WebEx Teams token here'
+SPARK_ROOM = 'IT Personal Assistant'  # to be used when manager arrives to office
+
+
+DNAC_URL = 'https://Your DNA Center'
+DNAC_USER = 'user'
+DNAC_PASS = 'password'
+
+
+SNOW_URL = 'https://your_service_now_instance.service-now.com/api/now'
+SNOW_ADMIN = 'admin'
+SNOW_DEV = 'IOSXE'
+SNOW_PASS = 'password'
+SNOW_INSTANCE = 'your_service_now_instance'
